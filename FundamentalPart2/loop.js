1be88145810389681422a6736514c55f4f7c865c
Azad Pratap Singh <iamcoolazad@gmail.com>
2024-12-07 16:10:17 +0530

    loop in javascript

---
+++ FundamentalPart2/loop.js
@@ -0,0 +1,19 @@
+for(let x=1;x<=5;x++){
+    console.log(`This is repitiation number ${x}  🥹 `)
+}
+
+
+const jonas=[
+    'Jonas',
+    'Singh',
+    2037-2045,
+    ['Azad','Pratap','Singh'],
+    true
+]
+const types=[]
+for(let y=0;y<jonas.length;y++){
+    console.log(jonas[y], typeof jonas[y])
+    types.push(typeof jonas[y])
+}
+
+console.log(types)
