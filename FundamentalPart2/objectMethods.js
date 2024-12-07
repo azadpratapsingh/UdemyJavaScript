4f132c117a9de4738a640fc9f21cc4da742797af
Azad Pratap Singh <iamcoolazad@gmail.com>
2024-12-07 14:49:47 +0530

    objecte methods completed

--- FundamentalPart2/objectMethods.js
+++ FundamentalPart2/objectMethods.js
@@ -8,7 +8,14 @@ const jonas={
     calcAge:function(){
         console.log(this)
         return 2037-this.birthYeah
+    },
+
+    getSummary:function(){
+        return `${this.firstName} is a ${this.calcAge()} -year old ${this.job} and he has ${this.hasDriverLicence ?'a':'no'} driver licence`
     }
+
+    
 }
 
 console.log(jonas.calcAge())
+console.log(jonas.getSummary())
